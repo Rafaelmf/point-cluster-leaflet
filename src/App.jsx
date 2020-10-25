1a305a2074b1885f5cf99a8d99455e5028ee92fc
RafaelFreitas <rafaelmf1801@gmail.com>
2020-10-25 16:35:16 -0300

    Setup initial layout with antd

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppHeader from './layout/AppHeader';
+import AppContent from './layout/AppContent';
+import AppFooter from './layout/AppFooter';
+
+const App = () => (
+  <div className="App">
+    <AppHeader />
+    <AppContent />
+    <AppFooter />
+  </div>
+);
+
+export default App;
---
+++ src/layout/AppContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const { Content } = Layout;
+
+const AppContent = () => {
+  return (
+    <Content className="site-layout">
+      <div className="site-layout-content">site-layout-content</div>
+    </Content>
+  );
+};
+
+export default AppContent;
